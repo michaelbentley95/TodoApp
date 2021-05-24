78f2f7dbdab40d6bb646980cb85629be765294ee
Michael Bentley <michaelbentley95@gmail.com>
2021-05-24 19:30:17 -0400

    Implemented CRUD operations for Item

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
 
 namespace TodoApp.Controllers
 {
@@ -6,10 +10,72 @@ namespace TodoApp.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult TestRun()
+        private readonly ApiDbContext _context;
+        public TodoController(ApiDbContext context)
         {
-            return Ok("success");
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetItems()
+        {
+            var items = await _context.Items.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(ItemData data)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Items.AddAsync(data);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetItem", new { data.Id }, data);
+            }
+            return new JsonResult("Something went wrong") { StatusCode = 500 };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, ItemData item)
+        {
+            if (item.Id != id)
+            {
+                return BadRequest();
+            }
+            var itemExists = await _context.Items.SingleOrDefaultAsync(x => x.Id == id);
+            if (itemExists is null)
+            {
+                return NotFound();
+            }
+            itemExists.Title = item.Title; //ToDo: Change to Automapper
+            itemExists.Description = item.Description;
+            itemExists.Done = item.Done;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return Ok(item);
         }
     }
 }
